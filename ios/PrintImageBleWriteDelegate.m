839e5b95f1510cbbbf45f2320ae446e14114e126
Alexander Engel <ae94@icloud.com>
2020-05-08 11:29:36 -0700

    fix

--- ios/PrintImageBleWriteDelegate.m
+++ ios/PrintImageBleWriteDelegate.m
@@ -15,7 +15,7 @@
     if(success){
         if (_now == -1) {
              if(_pendingResolve) {_pendingResolve(nil); _pendingResolve=nil;}
-        } else if(_now>=[_toPrint length]) {
+        } else if(_now >= [_toPrint length]) {
 //            ASCII ESC M 0 CR LF
 //            Hex 1B 4D 0 0D 0A
 //            Decimal 27 77 0 13 10
@@ -41,7 +41,7 @@
 -(void) print {
     @synchronized (self) {
         NSInteger sizePerLine = (int)(_width/8);
-        NSData *subData = [_toPrint subdataWithRange:NSMakeRange(_now, sizePerLine)];
+        NSData *subData = [_toPrint subdataWithRange:NSMakeRange(_now, MIN(sizePerLine, (_toPrint.length - _now)))];
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.01f)), dispatch_get_main_queue(), ^{
             [RNBluetoothManager writeValue:subData withDelegate:self];
             self.now = self.now + sizePerLine;
